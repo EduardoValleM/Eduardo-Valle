5305b914a300bb24f9f70fc7e6d4295b8c37b4f9
T- <T-@PC244>
2017-05-18 19:36:15 -0500

    clase del jueves 18 de mayo

--- src/eduardo_valle/Eduardo_Valle.java
+++ src/eduardo_valle/Eduardo_Valle.java
@@ -14,8 +14,18 @@ public class Eduardo_Valle {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        //inician pruebas de caja negra
+        //crearemos un usuario hipotetico
+        Usuario u= new Usuario();
+        //ahora le asignaremos valores
+        u.altura=1.86f;
+        u.peso=90f;
+        //Generaremos el modelo
+        Imc imc = new Imc();
+        
+        System.out.println("el valor es"+ imc.calcular(u) );
     }
     
 }
